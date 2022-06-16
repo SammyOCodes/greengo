d4cb8532fac908e15a83c9968025b006f54fe2b1
SammyOCodes <sammyocodes@gmail.com>
2022-06-16 16:05:07 -0500

    added firebase auth page, backend, button, dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:greengo/screens/home_screen.dart';
 import 'package:greengo/screens/login_screen.dart';
+import 'package:greengo/screens/authentication_screen.dart';
+
+import 'package:firebase_auth/firebase_auth.dart'; // new
+import 'package:firebase_core/firebase_core.dart'; // new
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';           // new
+
+import 'screens/auth_src/widgets.dart';
+import 'screens/auth_src/firebase_options.dart';                    // new
+import 'screens/auth_src/authentication.dart';                  // new
+import 'screens/auth_src/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, _) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +32,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFE3F4FF)),
-      home: HomeScreen(),
+      home: HomePage(),
     );
   }
 }
