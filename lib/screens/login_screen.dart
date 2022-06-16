dd3cb9a74ba7b99e787fc1961038e1cbdccef525
Shania <shaniatran101@gmail.com>
2022-06-16 15:05:52 -0500

    Login Screen changes
    Here you go sam!

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,14 +10,84 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  String userName = '';
+  TextEditingController passwordController = TextEditingController();
+  String passWord = '';
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Text('Login Page')],
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: const Text(
+                'J.P Morgan Chase & Co.',
+              style:TextStyle(color: Color(0xff238c00)),
+            ),
+          ),
+          body: Center(child: Column(children: <Widget>[
+            Container(
+                margin: const EdgeInsets.all(20),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    labelText: 'SSO ID',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      userName = text;
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Text(userName),
+            ),
+            Container(
+                margin: const EdgeInsets.all(20),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    labelText: 'Password',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      passWord = text;
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Text(passWord),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )
+            ),
+          ]))),
     );
   }
 }
