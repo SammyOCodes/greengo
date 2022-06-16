e5db0ab976b341b64f034eb682e4b88149239e70
tarzerk <its.krei@gmail.com>
2022-06-16 14:07:16 -0500

    welcome back

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -21,13 +22,48 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {},
             icon: Icon(Icons.ac_unit),
             color: Colors.blue,
+            iconSize: 40,
           )
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Welcome back'),
-          RichText(text: TextSpan(text: 'Jaime'))
+          Text(
+            '   Welcome Back',
+            style: TextStyle(
+              fontSize: 20,
+              color: Color(0xFF),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            ' Jaime!',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+              ),
+              CircularPercentIndicator(
+                animation: true,
+                radius: 100,
+                lineWidth: 25,
+                percent: .75,
+                progressColor: Color(0xFF2DD15B),
+                backgroundColor: Color.fromARGB(197, 186, 255, 206),
+                circularStrokeCap: CircularStrokeCap.round,
+              )
+            ],
+          )
         ],
       ),
     );
