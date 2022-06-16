26fbc1dde0ee03bd49dc56f810cddf8887c3b561
SammyOCodes <sammyocodes@gmail.com>
2022-06-16 16:17:59 -0500

    auth changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,7 @@ import 'package:greengo/screens/home_screen.dart';
 import 'package:greengo/screens/login_screen.dart';
 import 'package:greengo/screens/authentication_screen.dart';
 
-import 'package:firebase_auth/firebase_auth.dart'; // new
-import 'package:firebase_core/firebase_core.dart'; // new
-import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';           // new
-
-import 'screens/auth_src/widgets.dart';
-import 'screens/auth_src/firebase_options.dart';                    // new
-import 'screens/auth_src/authentication.dart';                  // new
-import 'screens/auth_src/widgets.dart';
 
 void main() {
   runApp(
